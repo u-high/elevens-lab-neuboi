461c626503e2be7ae5a5447e901c6bac9b2bfd17
Neuboi <sadjei1@lsu.edu>
2019-02-13 14:55:20 -0600

    Day One

--- Activity1 Starter Code/CardTester.java
+++ Activity1 Starter Code/CardTester.java
@@ -3,11 +3,19 @@
  */
 public class CardTester {
 
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
+ /**
+  * The main method in this class checks the Card operations for consistency.
+  * @param args is not used.
+  */
+ public static void main(String[] args) {
+  /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+   Card c = new Card("Diamonds", "Queen", 11);
+   Card cTwo = new Card("Spades", "Ten", 1);
+   Card cThree = new Card("Hearts", "Three", 3);
+   
+   System.out.println(c.toString());
+   System.out.println(cTwo.toString()); 
+   System.out.println(cThree.toString());  
+   
+ }
 }
